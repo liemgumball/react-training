27f273fc599dc752c7e077fcd622767afbcf7b4f
liemgumball <liem1762001@gmail.com>
2023-09-11 21:23:47 +0700

    feat: authentication

---
+++ practice-one/src/constants/regex.ts
@@ -0,0 +1,3 @@
+export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+export const passwordRegex =
+  /^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!*_])[A-Za-z\d@#$%^&+=!*_]{6,}$/
--- practice-one/vite.config.ts
+++ practice-one/vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig({
       '@features': '/src/features',
       '@components': '/src/components',
       '@hooks': '/src/hooks',
+      '@constants': '/src/constants',
+      '@services': '/src/services',
     },
   },
 })
