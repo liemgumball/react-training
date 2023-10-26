a2e2d0018540f262303190268a72c3683183d895
liemgumball <liem1762001@gmail.com>
2023-10-26 10:44:55 +0700

    feat: configure storybook & vitest

--- practice-two/vite.config.ts
+++ practice-two/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/test/setup.ts",
+		css: true,
+		includeSource: ["src/**/*.{js,ts,jsx,tsx}"],
+	},
+});
