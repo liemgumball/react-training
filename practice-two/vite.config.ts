52b0bf5ec17ea304ec2a9082cc17c1144eb8515e
liemgumball <liem1762001@gmail.com>
2023-10-26 10:56:05 +0700

    feat: wdyr & prettier

--- practice-two/vite.config.ts
+++ practice-two/vite.config.ts
@@ -1,16 +1,16 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/test/setup.ts",
-		css: true,
-		includeSource: ["src/**/*.{js,ts,jsx,tsx}"],
-	},
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+    includeSource: ['src/**/*.{js,ts,jsx,tsx}'],
+  },
 });
