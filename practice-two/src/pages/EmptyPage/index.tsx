1b672e5fa1744512fda9a8c22811681ecec1f0aa
liemgumball <liem1762001@gmail.com>
2023-11-16 14:13:15 +0700

    feat: reset search on navigation

--- practice-two/src/pages/EmptyPage/index.tsx
+++ practice-two/src/pages/EmptyPage/index.tsx
@@ -1,5 +1,25 @@
+import Button from '@components/Button';
+import { useNavigate } from 'react-router-dom';
+
 const EmptyPage = () => {
-  return <div>EmptyPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="absolute inset-0 flex flex-col gap-5 items-center justify-center">
+      <h1 className="font-600 text-4xl my-5">
+        This is a feature in the future
+      </h1>
+      <Button
+        className="capitalize"
+        variant="primary"
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        return to home page
+      </Button>
+    </div>
+  );
 };
 
 export default EmptyPage;
