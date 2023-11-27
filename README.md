04a292564e68d7e17b0c77530f88970dd08b2985
liemgumball <liem1762001@gmail.com>
2023-11-27 11:47:45 +0700

    feat: create local server & update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# react-training
+# React-training
+
+-   Dev: Liem Nguyen
+-   Email: liem1762001@gmail.com
+-   Github: [liemgumball](https://github.com/liemgumball)
+
+## Develop Environment:
+
+-   [Visual Studio Code](https://code.visualstudio.com/)
+-   [Github](https://github.com/)
+-   React & TypeScript
---
+++ data/index.js
@@ -0,0 +1,59 @@
+const jsonServer = require("json-server"); // importing json-server library
+const auth = require("json-server-auth");
+
+const server = jsonServer.create();
+const router = jsonServer.router("data/db.json");
+
+const localIpAddress = require("local-ip-address");
+const Logger = require("@ptkdev/logger");
+const port = process.env.PORT || 3000; // use any port number here
+const logger = new Logger();
+const middlewares = jsonServer.defaults({ logger: true });
+
+// Bind the router db to the app
+server.db = router.db;
+
+server.use(middlewares);
+server.use(auth);
+
+server.use(jsonServer.bodyParser);
+
+// add created At
+server.use((req, res, next) => {
+	if (req.method === "POST") {
+		req.body.createdAt = new Date().toISOString();
+	}
+	// Continue to JSON Server router
+	next();
+});
+
+// add Avatar if POST to resource students
+server.use("/students", (req, res, next) => {
+	if (req.method === "POST") {
+		req.body.avatar = "https://loremflickr.com/60/60";
+	}
+	// Continue to JSON Server router
+	next();
+});
+
+// Reverse data for GET requests with _sort=createdAt
+server.use((req, res, next) => {
+	if (req.method === "GET" && req.query._sort === "createdAt") {
+		// Save the original send function
+		const originalSend = res.send;
+
+		// Check if _order is not already present in the request
+		if (!req.query._order) {
+			req.query._order = "desc";
+		}
+	}
+	next();
+});
+
+server.use(router);
+
+server.listen(port, () => {
+	logger.info(
+		`JSON Server is running - http://localhost:${port} - http://${localIpAddress()}:${port}`
+	);
+});
